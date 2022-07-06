913f3f7799833dca4e5085fe8152873eb053f7f3
manaspradhan1 <pradhanmanas091@gmail.com>
2022-07-06 14:31:56 +0530

    First Commit

---
+++ src/com/paytm/resource/PayTMWallet.java
@@ -0,0 +1,15 @@
+package com.paytm.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("/wallet")
+public class PayTMWallet {
+	@GET
+	@Produces("text/plain")
+	public double getBalance(@QueryParam("mobileNo") String mobileNo) {
+		return 334.34;
+	}
+}
